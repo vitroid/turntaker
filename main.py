0872e012f1453419101292f4a1c449bb1e668978
vitroid <vitroid@gmail.com>
2024-07-01 10:26:00 +0900

    -

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ from fastapi.templating import Jinja2Templates
 
 if platform.uname().node == "vitroid-brass.local":
     # ローカルデバッグ。
-    BASEURL="http://127.0.0.1:8000"
-    BASEPATH="."
+    BASEURL = "http://127.0.0.1:8000"
+    BASEPATH = "."
 else:
     # Deployする場合。(環境変数でとれないかな)
-    BASEURL="https://turntaker.fly.dev"
-    BASEPATH="/data"
+    BASEURL = "https://turntaker.fly.dev"
+    BASEPATH = "/data"
 
 basicConfig(level=DEBUG)
 logger = getLogger()
@@ -28,11 +28,13 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-favicon_path = 'static/favicon.svg'
+favicon_path = "static/favicon.svg"
 
-@app.get('/favicon.svg', include_in_schema=False)
+
+@app.get("/favicon.svg", include_in_schema=False)
 async def favicon():
     return FileResponse(favicon_path)
+
 
 origins = ["*"]
 
@@ -57,7 +59,7 @@ def read_root():
 def get_random_string(length):
     # choose from all lowercase letter
     letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(length))
+    return "".join(random.choice(letters) for i in range(length))
 
 
 def new_occasion():
@@ -65,12 +67,14 @@ def new_occasion():
     while True:
         admin = get_random_string(8)
         if len(occasions.search(q.admin == admin)) == 0:
-            record = {"admin": admin,
-                      "occasion_id": get_random_string(8),
-                      "smartphone_mode": 0,
-                      "count": 1,
-                      "waiting": 1,
-                      "title": "Title"}
+            record = {
+                "admin": admin,
+                "occasion_id": get_random_string(8),
+                "smartphone_mode": 0,
+                "count": 1,
+                "waiting": 1,
+                "title": "Title",
+            }
             occasions.insert(record)
             return record
 
@@ -87,7 +91,7 @@ def new():
 
 
 @app.get("/q/{token}")
-def query(token:str):
+def query(token: str):
     q = ti.Query()
     trecords = tokens.search(q.token == token)
     if len(trecords) == 0:
@@ -98,15 +102,17 @@ def query(token:str):
     records = occasions.search(q.occasion_id == occasion_id)
     record = records[0]
     record["admin"] = ""
-    return {"token": token,
-            "title": record["title"],
-            "count": record["count"],
-            "mode": record["smartphone_mode"],
-            "waiting": trecords[0]["waiting"]}
+    return {
+        "token": token,
+        "title": record["title"],
+        "count": record["count"],
+        "mode": record["smartphone_mode"],
+        "waiting": trecords[0]["waiting"],
+    }
 
 
 @app.get("/r/{occasion_id}")
-def reserve(occasion_id:str):
+def reserve(occasion_id: str):
     q = ti.Query()
     records = occasions.search(q.occasion_id == occasion_id)
     if len(records) == 0:
@@ -118,21 +124,22 @@ def reserve(occasion_id:str):
     while True:
         token = get_random_string(8)
         if len(tokens.search(q.token == token)) == 0:
-            trecord = {"token": token,
-                      "occasion_id": occasion_id,
-                      "waiting": record["waiting"]}
+            trecord = {
+                "token": token,
+                "occasion_id": occasion_id,
+                "waiting": record["waiting"],
+            }
             tokens.insert(trecord)
 
             # 待ち番号を1増やす。
             waiting = record["waiting"] + 1
-            occasions.update({"waiting": waiting},
-                             q.occasion_id == occasion_id)
+            occasions.update({"waiting": waiting}, q.occasion_id == occasion_id)
 
             return trecord
 
 
 @app.get("/set/{admin}/{count}")
-def set_count(admin:str, count:int):
+def set_count(admin: str, count: int):
     q = ti.Query()
     records = occasions.search(q.admin == admin)
     if len(records) == 0:
@@ -149,7 +156,7 @@ def set_count(admin:str, count:int):
 
 
 @app.get("/t/{admin}/{title}")
-def set_title(admin:str, title:str):
+def set_title(admin: str, title: str):
     q = ti.Query()
     records = occasions.search(q.admin == admin)
     if len(records) == 0:
@@ -165,7 +172,7 @@ def set_title(admin:str, title:str):
 
 
 @app.get("/s/{admin}/{mode}")
-def set_smartphone_mode(admin:str, mode:int):
+def set_smartphone_mode(admin: str, mode: int):
     q = ti.Query()
     records = occasions.search(q.admin == admin)
     if len(records) == 0:
@@ -182,38 +189,43 @@ def set_smartphone_mode(admin:str, mode:int):
 
 # 一応APIとして作ったけど、UIを別に準備するのが面倒なので、その場表示もする。
 @app.get("/customer/{token}", response_class=HTMLResponse)
-async def query_UI(request:Request, token:str):
+async def query_UI(request: Request, token: str):
     trecord = query(token)
-    return templates.TemplateResponse("Q.html", {"request": request,
-                                                 "baseurl": BASEURL}
-                                                 | trecord)
+    return templates.TemplateResponse(
+        "Q.html", {"request": request, "baseurl": BASEURL} | trecord
+    )
 
 
 @app.get("/", response_class=RedirectResponse)
-async def new_UI(request:Request):
+async def new_UI(request: Request):
     record = new()
     admin = record["admin"]
     return RedirectResponse(f"{BASEURL}/admin/{admin}")
 
 
 @app.get("/Set/{admin}/{count}", response_class=RedirectResponse)
-async def set_UI(admin:str, count:int, request:Request):
+async def set_UI(admin: str, count: int, request: Request):
     record = set_count(admin, count)
     return RedirectResponse(f"{BASEURL}/admin/{admin}")
 
 
 @app.get("/admin/{admin}", response_class=HTMLResponse)
-async def admin_UI(admin:str, request:Request):
+async def admin_UI(admin: str, request: Request):
     record = set_count(admin, 0)
-    return templates.TemplateResponse("A.html", {"request": request,
-                                                 "inc": record["count"]+1,
-                                                 "dec": record["count"]-1,
-                                                 "baseurl": BASEURL}
-                                                 | record)
+    return templates.TemplateResponse(
+        "A.html",
+        {
+            "request": request,
+            "inc": record["count"] + 1,
+            "dec": record["count"] - 1,
+            "baseurl": BASEURL,
+        }
+        | record,
+    )
 
 
 @app.get("/R/{occasion_id}", response_class=RedirectResponse)
-async def reserve_UI(request:Request, occasion_id:str):
+async def reserve_UI(request: Request, occasion_id: str):
     trecord = reserve(occasion_id)
     token = trecord["token"]
     return RedirectResponse(f"{BASEURL}/customer/{token}")
